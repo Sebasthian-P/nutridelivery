9133c48a5ec24ba66559908f0cb43f76c8391090
Sebastian <plsebas25@gmail.com>
2023-11-29 09:24:42 -0500

    version final

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,7 +3,13 @@
 let closeBtn = document.getElementById('close');
 let menu = document.getElementById('nav');
 let openBtn = document.getElementById('open');
-let btn = document.querySelectorAll('btn__services')
+let btn = document.querySelectorAll('btn__services');
+
+let btnCloseForm = document.getElementById('btn__close');
+let btnOpenForm = document.getElementById('btn__open');
+let form = document.getElementById('form');
+
+
 
 const openMenu = () => {
     menu.style.transform = 'translateX(0%)';
@@ -16,5 +22,17 @@ const closeMenu = () => {
     document.body.style.overflow = 'visible';
 }
 
+const closeForm = () => {
+    form.style.transform = 'translate(1400px)';
+    form.style.zIndex = '';
+}
+
+const openForm = () => {
+    form.style.transform = 'translate(0)';
+    form.style.zIndex = '10000';
+}
+
 openBtn.onclick = openMenu;
 closeBtn.onclick = closeMenu;
+btnCloseForm.onclick = closeForm;
+btnOpenForm.onclick = openForm
